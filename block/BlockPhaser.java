f1fae6df3b7caa8cb5e2bd3be8de5b1ca2cace10
Fred4106 <natruf@gmail.com>
2013-03-30 19:46:45 -0500

    Committed on Sat Mar 30 19:46:45 CDT 2013

--- block/BlockPhaser.java
+++ block/BlockPhaser.java
@@ -3,14 +3,24 @@ package massiveOctoAdventure.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.Icon;
 
 public class BlockPhaser extends Block{
 
+	private Icon frontTexture;
+	private Icon otherTexture;
+	
 	public BlockPhaser(int id) {
 		super(id, Material.iron);
 		setCreativeTab(CreativeTabs.tabRedstone);
 		setHardness(1F);
 		setResistance(1F);
+	}
+	
+	@Override
+	public void registerIcons(IconRegister register) {
+		frontTexture = register.registerIcon("massiveoctoadventure:phaserFront");
+		otherTexture = register.registerIcon("massiveoctoadventure:phaserFront");
 	}
 	
 }
