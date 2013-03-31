d9f39e776ab5c4ebbd235a59fdf2ccfa2a61b249
Fred4106 <natruf@gmail.com>
2013-03-31 03:43:15 -0500

    Fell asleep at computer.  Phaser works.

--- moa_common/block/TilePhaser.java
+++ moa_common/block/TilePhaser.java
@@ -5,11 +5,16 @@ import net.minecraft.tileentity.TileEntity;
 
 public class TilePhaser extends TileEntity{
 	private boolean hasPlan = false;
-	private int cubeSize = 3;// the actual cube will be twice this number + 1.  The center will be the phaserBlock
+	private int cubeSize = 7;// the actual cube will be twice this number + 1.  The center will be the phaserBlock
 	private utilBlockInfo[] storage = new utilBlockInfo[(int) Math.pow(((cubeSize*2) + 1),3)];
+	private utilBlockInfo[] storageAlt = new utilBlockInfo[(int) Math.pow(((cubeSize*2) + 1),3)];
 	
 	public boolean hasPlan() {
 		return hasPlan;
+	}
+	
+	private void sendBlockUpdate(int x, int y, int z) {
+		worldObj.notifyBlocksOfNeighborChange(x, y, z, worldObj.getBlockId(x, y, z));
 	}
 	
 	public void scanPlan() {
@@ -17,30 +22,167 @@ public class TilePhaser extends TileEntity{
 		int tempX;
 		int tempY;
 		int tempZ;
-		for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
-			for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
-				for(int y = this.yCoord+1; y <= this.yCoord+(cubeSize*2+1); y++) {
-					tempX = Math.abs(x - this.xCoord-cubeSize);
-					tempY = Math.abs(y - this.yCoord+1);
-					tempZ = Math.abs(z - this.zCoord-cubeSize);
-					storage[tempY+tempZ*tempY+tempZ*tempY*tempX] = new utilBlockInfo(x, y, z, worldObj.getBlockId(x, y, z), worldObj.getBlockMetadata(x, y, z));
+		System.out.println("Scanning");
+		if(meta == 0) {
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int y = this.yCoord-(cubeSize*2+1); y <= this.yCoord-1; y++) {
+						tempX = Math.abs(x - (this.xCoord-cubeSize));
+						tempY = Math.abs(y - (this.yCoord-(cubeSize*2+1)));
+						tempZ = Math.abs(z - (this.zCoord-cubeSize));
+						storage[tempX+tempZ*(cubeSize*2+1)+tempY*(cubeSize*2+1)*(cubeSize*2+1)] = new utilBlockInfo(x, y, z, worldObj.getBlockId(x, y, z), worldObj.getBlockMetadata(x, y, z));
+					}
+				}
+			}
+		} else if(meta == 1) {
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int y = this.yCoord+1; y <= this.yCoord+(cubeSize*2+1); y++) {
+						tempX = Math.abs(x - (this.xCoord-cubeSize));
+						tempY = Math.abs(y - (this.yCoord+1));
+						tempZ = Math.abs(z - (this.zCoord-cubeSize));
+						storage[tempX+tempZ*(cubeSize*2+1)+tempY*(cubeSize*2+1)*(cubeSize*2+1)] = new utilBlockInfo(x, y, z, worldObj.getBlockId(x, y, z), worldObj.getBlockMetadata(x, y, z));
+					}
+				}
+			}
+		} else if(meta == 2) {
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+					for(int z = this.zCoord-(cubeSize*2+1); z <= this.zCoord-1; z++) {
+						tempX = Math.abs(x - (this.xCoord-cubeSize));
+						tempZ = Math.abs(z - (this.zCoord-(cubeSize*2+1)));
+						tempY = Math.abs(y - (this.yCoord-cubeSize));
+						storage[tempX+tempY*(cubeSize*2+1)+tempZ*(cubeSize*2+1)*(cubeSize*2+1)] = new utilBlockInfo(x, y, z, worldObj.getBlockId(x, y, z), worldObj.getBlockMetadata(x, y, z));
+					}
+				}
+			}
+		} else if(meta == 3) {
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+					for(int z = this.zCoord+1; z <= this.zCoord+(cubeSize*2+1); z++) {
+						tempX = Math.abs(x - (this.xCoord-cubeSize));
+						tempZ = Math.abs(z - (this.zCoord+1));
+						tempY = Math.abs(y - (this.yCoord-cubeSize));
+						storage[tempX+tempY*(cubeSize*2+1)+tempZ*(cubeSize*2+1)*(cubeSize*2+1)] = new utilBlockInfo(x, y, z, worldObj.getBlockId(x, y, z), worldObj.getBlockMetadata(x, y, z));
+					}
+				}
+			}
+		} else if(meta == 4) {
+			for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int x = this.xCoord-(cubeSize*2+1); x <= this.xCoord-1; x++) {
+						tempY = Math.abs(y - (this.yCoord-cubeSize));
+						tempX = Math.abs(x - (this.xCoord-(cubeSize*2+1)));
+						tempZ = Math.abs(z - (this.zCoord-cubeSize));
+						storage[tempX+tempZ*(cubeSize*2+1)+tempY*(cubeSize*2+1)*(cubeSize*2+1)] = new utilBlockInfo(x, y, z, worldObj.getBlockId(x, y, z), worldObj.getBlockMetadata(x, y, z));
+					}
+				}
+			}
+		} else if(meta == 5) {
+			for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int x = this.xCoord+1; x <= this.xCoord+(cubeSize*2+1); x++) {
+						tempY = Math.abs(y - (this.yCoord-cubeSize));
+						tempX = Math.abs(x - (this.xCoord+1));
+						tempZ = Math.abs(z - (this.zCoord-cubeSize));
+						storage[tempX+tempZ*(cubeSize*2+1)+tempY*(cubeSize*2+1)*(cubeSize*2+1)] = new utilBlockInfo(x, y, z, worldObj.getBlockId(x, y, z), worldObj.getBlockMetadata(x, y, z));
+					}
 				}
 			}
 		}
+		this.hasPlan = true;
 	}
 	
 	public void removeProperArea() {
 		int meta = worldObj.getBlockMetadata(xCoord, yCoord, zCoord);
-		int tempX;
-		int tempY;
-		int tempZ;
-		for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
-			for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
-				for(int y = this.yCoord+1; y <= this.yCoord+(cubeSize*2+1); y++) {
-					tempX = x - this.xCoord-cubeSize;
-					tempY = y - this.yCoord+1;
-					tempZ = z - this.zCoord-cubeSize;
-					worldObj.setBlock(x, y, z, 0, 0, 0x02);
+		if(meta == 0) {
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int y = this.yCoord-(cubeSize*2+1); y <= this.yCoord-1; y++) {
+						worldObj.setBlock(x, y, z, 0, 0, 0x02);
+					}
+				}
+			}
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int y = this.yCoord-(cubeSize*2+1); y <= this.yCoord-1; y++) {
+						worldObj.notifyBlocksOfNeighborChange(x, y, z, this.getBlockType().blockID);
+					}
+				}
+			}
+		} else if(meta == 1) {
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int y = this.yCoord+1; y <= this.yCoord+(cubeSize*2+1); y++) {
+						worldObj.setBlock(x, y, z, 0, 0, 0x02);
+					}
+				}
+			}
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int y = this.yCoord+1; y <= this.yCoord+(cubeSize*2+1); y++) {
+						worldObj.notifyBlocksOfNeighborChange(x, y, z, this.getBlockType().blockID);
+					}
+				}
+			}
+		} else if(meta == 2) {
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+					for(int z = this.zCoord-(cubeSize*2+1); z <= this.zCoord-1; z++) {
+						worldObj.setBlock(x, y, z, 0, 0, 0x02);
+					}
+				}
+			}
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+					for(int z = this.zCoord-(cubeSize*2+1); z <= this.zCoord-1; z++) {
+						worldObj.notifyBlocksOfNeighborChange(x, y, z, this.getBlockType().blockID);
+					}
+				}
+			}
+		} else if(meta == 3) {
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+					for(int z = this.zCoord+1; z <= this.zCoord+(cubeSize*2+1); z++) {
+						worldObj.setBlock(x, y, z, 0, 0, 0x02);
+					}
+				}
+			}
+			for(int x = this.xCoord-cubeSize; x <= this.xCoord+cubeSize; x++) {
+				for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+					for(int z = this.zCoord+1; z <= this.zCoord+(cubeSize*2+1); z++) {
+						worldObj.notifyBlocksOfNeighborChange(x, y, z, this.getBlockType().blockID);
+					}
+				}
+			}
+		} else if(meta == 4) {
+			for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int x = this.xCoord-(cubeSize*2+1); x <= this.xCoord-1; x++) {
+						worldObj.setBlock(x, y, z, 0, 0, 0x02);
+					}
+				}
+			}
+			for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int x = this.xCoord-(cubeSize*2+1); x <= this.xCoord-1; x++) {
+						worldObj.notifyBlocksOfNeighborChange(x, y, z, this.getBlockType().blockID);
+					}
+				}
+			}
+		} else if(meta == 5) {
+			for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int x = this.xCoord+1; x <= this.xCoord+(cubeSize*2+1); x++) {
+						worldObj.setBlock(x, y, z, 0, 0, 0x02);
+					}
+				}
+			}
+			for(int y = this.yCoord-cubeSize; y <= this.yCoord+cubeSize; y++) {
+				for(int z = this.zCoord-cubeSize; z <= this.zCoord+cubeSize; z++) {
+					for(int x = this.xCoord+1; x <= this.xCoord+(cubeSize*2+1); x++) {
+						worldObj.notifyBlocksOfNeighborChange(x, y, z, this.getBlockType().blockID);
+					}
 				}
 			}
 		}
@@ -48,8 +190,10 @@ public class TilePhaser extends TileEntity{
 	
 	public void placePlan() {
 		for(int a = 0; a < storage.length; a++) {
-			this.worldObj.setBlock(storage[a].x, storage[a].y, storage[a].z, storage[a].id, storage[a].meta, 0x02);
-			this.worldObj.markBlockForUpdate(storage[a].x, storage[a].y, storage[a].z);
+			if(storage[a] != null) {
+				this.worldObj.setBlock(storage[a].x, storage[a].y, storage[a].z, storage[a].id, storage[a].meta, 0x02);
+				this.worldObj.markBlockForUpdate(storage[a].x, storage[a].y, storage[a].z);
+			}
 		}
 		this.hasPlan = false;
 	}
