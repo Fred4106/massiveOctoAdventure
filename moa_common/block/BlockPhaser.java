1ec98f575f99db7d47e3b8c3614ababf15aed716
Fred4106 <natruf@gmail.com>
2013-03-30 21:07:29 -0500

    Committed  on Sat Mar 30 21:07:29 CDT 2013

--- moa_common/block/BlockPhaser.java
+++ moa_common/block/BlockPhaser.java
@@ -1,5 +1,7 @@
 package massiveOctoAdventure.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
@@ -9,8 +11,9 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.ForgeDirection;
 
 public class BlockPhaser extends Block{
-
+	@SideOnly(Side.CLIENT)
 	private Icon frontTexture;
+	@SideOnly(Side.CLIENT)
 	private Icon otherTexture;
 	
 	public BlockPhaser(int id) {
@@ -21,15 +24,19 @@ public class BlockPhaser extends Block{
 	}
 	
 	@Override
-	public Icon getBlockTexture(IBlockAccess world, int x, int y, int z, int side) {
-		ForgeDirection sideForge = ForgeDirection.getOrientation(side);
-		return frontTexture;
+	public Icon getBlockTextureFromSideAndMetadata(int side, int meta) {
+		if(side == 1) {
+			return frontTexture;
+		} else {
+			return otherTexture;
+		}
 	}
 	
+	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerIcons(IconRegister register) {
 		frontTexture = register.registerIcon("massiveoctoadventure:phaserFront");
-		otherTexture = register.registerIcon("massiveoctoadventure:phaserFront");
+		otherTexture = register.registerIcon("massiveoctoadventure:phaserSide");
 	}
 	
 }
